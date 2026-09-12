b83e0485800de77cda37ddca2b65a197361d26a9
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2026-09-11 23:57:06 -0300

    feat: add event auto generation when project starts

--- src/main/java/com/cauanlagrotta/config/DynamoDbConfig.java
+++ src/main/java/com/cauanlagrotta/config/DynamoDbConfig.java
@@ -5,11 +5,16 @@ import java.net.URI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.cauanlagrotta.entity.Event;
+
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.ResourceNotFoundException;
 
 @Configuration 
 public class DynamoDbConfig {
@@ -29,5 +34,18 @@ public class DynamoDbConfig {
         return DynamoDbEnhancedClient.builder()
             .dynamoDbClient(dynamoDbClient)
             .build();
+    }
+
+    @Bean
+    public DynamoDbTable<Event> eventTable(DynamoDbEnhancedClient client){
+
+        DynamoDbTable<Event> table = client.table("Events", TableSchema.fromBean(Event.class));
+
+        try{
+            table.describeTable();
+        }catch(ResourceNotFoundException e){
+            table.createTable();
+        }
+        return table;
     }
 }
